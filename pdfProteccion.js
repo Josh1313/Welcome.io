caa1de3ad1c83adc758a1df729cf17e356606620
josue <licmartinez827@gmail.com.com>
2024-01-21 13:26:54 +0100

    third commit

--- pdfProteccion.js
+++ pdfProteccion.js
@@ -1,3 +1,18 @@
+// document.addEventListener('DOMContentLoaded', function() {
+//     const pdfFrame = document.getElementById('pdfFrame');
+    
+//     pdfFrame.addEventListener('contextmenu', function(e) {
+//         e.preventDefault();
+//     });
+
+//     pdfFrame.contentWindow.document.addEventListener('contextmenu', function(e) {
+//         e.preventDefault();
+//     });
+
+//     pdfFrame.addEventListener('mousedown', function(e) {
+//         e.preventDefault();
+//     });
+// });
 document.addEventListener('DOMContentLoaded', function() {
     const pdfFrame = document.getElementById('pdfFrame');
     
@@ -12,5 +27,8 @@ document.addEventListener('DOMContentLoaded', function() {
     pdfFrame.addEventListener('mousedown', function(e) {
         e.preventDefault();
     });
+
+    // Añade la siguiente línea para hacer que el contenido del PDF sea responsive
+    pdfFrame.contentWindow.document.querySelector('body').style.width = '100%';
 });
 
